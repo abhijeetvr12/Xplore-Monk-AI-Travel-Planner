5aa658b9233b44a58781de9df300842082a42fe3
Abhijeet Kumar <abhijeetvr12@gmail.com>
2025-01-18 17:29:16 +0530

    Update Start button

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { Button } from '../ui/button';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
-    <div className="relative flex flex-col items-center mx-56 gap-9">
-      <div className="relative z-10 flex flex-col items-center gap-9">
+    <div className='flex flex-col items-center mx-56 gap-9'>
         <h1 className="font-extrabold text-[50px] text-center mt-16">
           <span className="text-[#f56551]">XploreMonk:</span> Your Gateway to Unforgettable Journeys
         </h1>
-        <p className="text-xl text-gray-500 text-center">
+    <p className="text-xl text-gray-500 text-center">
           Unlock the magic of travel with AI-powered, tailor-made itineraries. 
           Let XploreMonk curate experiences that perfectly align with your passions, pace, and pocket.
         </p>
-        <Link to={'/create-trip'}>
-          <Button>Start Planning Now – It's Free!</Button>
-        </Link>
+    <Link to={'/create-trip'}>
+    <Button className=''>Start Planning Now – It's Free!</Button>
+    </Link>
 
-        <img src='/laptop.png' alt="" className='-mt-20' />
-      </div>
+    <img src='/laptop.png' alt="" className='-mt-15' />
     </div>
-  );
+    
+  )
 }
 
-export default Hero;
+export default Hero 
